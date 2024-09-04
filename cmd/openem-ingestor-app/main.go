e984d2044a035359e2c33a925019dd9c3e17833e
Philipp Wissmann <philipp.wissmann@ethz.ch>
2024-09-04 15:30:22 +0200

    App: Don't fail when config is missing

--- cmd/openem-ingestor-app/main.go
+++ cmd/openem-ingestor-app/main.go
@@ -19,7 +19,7 @@ var assets embed.FS
 func main() {
 
 	if err := core.ReadConfig(); err != nil {
-		panic(fmt.Errorf("Failed to read config file: %w", err))
+		log.Print(fmt.Errorf("failed to read config file: %w", err))
 	}
 	log.Printf("Config file used: %s", viper.ConfigFileUsed())
 	log.Println(viper.AllSettings())
