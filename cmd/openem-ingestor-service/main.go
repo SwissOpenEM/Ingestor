59dd3c329c9cce864145cd075f1e0b8ecbe91934
Philipp Wissmann <philipp.wissmann@ethz.ch>
2024-09-09 15:02:59 +0200

    Add goreleaser to ci pipeline and add config file

--- cmd/openem-ingestor-service/main.go
+++ cmd/openem-ingestor-service/main.go
@@ -9,7 +9,12 @@ import (
 	"github.com/spf13/viper"
 )
 
+// String can be overwritten by using linker flags: -ldflags "-X main.version=VERSION"
+var version string = "DEVELOPMENT_VERSION"
+
 func main() {
+	log.Printf("Version %s", version)
+
 	if err := core.ReadConfig(); err != nil {
 		panic(fmt.Errorf("Failed to read config file: %w", err))
 	}
