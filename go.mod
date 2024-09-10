6bff7025a2253206c968eb759c9b9104df5a6941
Philipp Wissmann <philipp.wissmann@ethz.ch>
2024-09-10 18:03:48 +0200

    API: add swagger ui docs page
    Create docs from function annoations to be served under /docs/index.html

--- go.mod
+++ go.mod
@@ -16,12 +16,16 @@ require (
 	github.com/oapi-codegen/runtime v1.1.1
 	github.com/paulscherrerinstitute/scicat-cli v0.1.6-0.20240905105622-f6eaf0dd713f
 	github.com/spf13/viper v1.19.0
+	github.com/swaggo/files v1.0.1
+	github.com/swaggo/gin-swagger v1.6.0
+	github.com/swaggo/swag v1.16.3
 	github.com/wailsapp/wails/v2 v2.9.1
 	golang.org/x/oauth2 v0.19.0
 )
 
 require (
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
+	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
@@ -40,6 +44,7 @@ require (
 )
 
 require (
+	github.com/KyleBanks/depth v1.2.1 // indirect
 	github.com/Netflix/go-expect v0.0.0-20220104043353-73e0943537d2 // indirect
 	github.com/apapsch/go-jsonmerge/v2 v2.0.0 // indirect
 	github.com/bep/debounce v1.2.1 // indirect
@@ -54,13 +59,14 @@ require (
 	github.com/go-ini/ini v1.67.0 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
 	github.com/go-openapi/jsonpointer v0.21.0 // indirect
+	github.com/go-openapi/jsonreference v0.21.0 // indirect
+	github.com/go-openapi/spec v0.21.0 // indirect
 	github.com/go-openapi/swag v0.23.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.22.1 // indirect
 	github.com/goccy/go-json v0.10.3 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
-	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/invopop/yaml v0.2.0 // indirect
 	github.com/jchv/go-winloader v0.0.0-20210711035445-715c2860da7e // indirect
 	github.com/josharian/intern v1.0.0 // indirect
--- internal/webserver/api.go
+++ internal/webserver/api.go
@@ -1,6 +1,7 @@
 //go:build go1.22
 
 //go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen --config=cfg.yaml ../../api/openapi.yaml
+//go:generate swag init -g api.go --pdl 7 -o ../../docs
 package webserver
 
 import (
@@ -15,11 +16,25 @@ type IngestorWebServerImplemenation struct {
 	version string
 }
 
+//	@contact.name	SwissOpenEM
+//	@contact.url	https://swissopenem.github.io
+//	@contact.email	spencer.bliven@psi.ch
+
+// @license.name	Apache 2.0
+// @license.url	http://www.apache.org/licenses/LICENSE-2.0.html
+
 func NewIngestorWebServer(version string) *IngestorWebServerImplemenation {
 	return &IngestorWebServerImplemenation{version: version}
 }
 
 // DatasetControllerIngestDataset implements ServerInterface.
+//
+//	@Description	Ingest a new dataset
+//	@Tags			datasets
+//	@Accept			json
+//	@Produce		json
+//
+//	@Router			/datasets [post]
 func (i *IngestorWebServerImplemenation) DatasetControllerIngestDataset(c *gin.Context) {
 	var result IngestorUiPostDatasetResponse
 
@@ -27,6 +42,13 @@ func (i *IngestorWebServerImplemenation) DatasetControllerIngestDataset(c *gin.C
 }
 
 // OtherControllerGetVersion implements ServerInterface.
+//
+//	@Description	Get the used ingestor version
+//	@Tags			other
+//	@Accept			json
+//	@Produce		json
+//
+//	@Router			/version [get]
 func (i *IngestorWebServerImplemenation) OtherControllerGetVersion(c *gin.Context) {
 	var result IngestorUiOtherVersionResponse
 	result.Version = &i.version
@@ -35,6 +57,13 @@ func (i *IngestorWebServerImplemenation) OtherControllerGetVersion(c *gin.Contex
 }
 
 // TransferControllerDeleteTransfer implements ServerInterface.
+//
+//	@Description	Cancel a data transfer
+//	@Tags			transfer
+//	@Accept			json
+//	@Produce		json
+//
+//	@Router			/transfer [delete]
 func (i *IngestorWebServerImplemenation) TransferControllerDeleteTransfer(c *gin.Context) {
 	var result IngestorUiDeleteTransferResponse
 
@@ -42,6 +71,14 @@ func (i *IngestorWebServerImplemenation) TransferControllerDeleteTransfer(c *gin
 }
 
 // TransferControllerGetTransfer implements ServerInterface.
+//
+//	@Description	Get list of transfers. Optional use the transferId parameter to only get one item.
+//	@Tags			transfer
+//	@Accept			json
+//	@Produce		json
+//	@param			params	path	TransferControllerGetTransferParams	true	"params"
+//
+//	@Router			/transfer [get]
 func (i *IngestorWebServerImplemenation) TransferControllerGetTransfer(c *gin.Context, params TransferControllerGetTransferParams) {
 	var result IngestorUiGetTransferResponse
 
