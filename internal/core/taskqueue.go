74b29b58581566f9bb4f66ac0ceaeb25ffc2d06e
consolethinks <consolethinks@gmail.com>
2024-09-24 17:14:27 +0200

    add status attribute to tasks

--- internal/core/taskqueue.go
+++ internal/core/taskqueue.go
@@ -8,54 +8,23 @@ import (
 	"sync"
 	"time"
 
+	"github.com/SwissOpenEM/Ingestor/internal/task"
 	"github.com/google/uuid"
 )
 
 type TaskQueue struct {
-	datasetUploadTasks sync.Map           // Datastructure to store all the upload requests
-	inputChannel       chan IngestionTask // Requests to upload data are put into this channel
-	resultChannel      chan TaskResult    // The result of the upload is put into this channel
+	datasetUploadTasks sync.Map                // Datastructure to store all the upload requests
+	inputChannel       chan task.IngestionTask // Requests to upload data are put into this channel
+	resultChannel      chan task.Result        // The result of the upload is put into this channel
 	AppContext         context.Context
 	Config             Config
 	Notifier           ProgressNotifier
 }
 
-type TransferMethod int
-
-const (
-	TransferS3 TransferMethod = iota + 1
-	TransferGlobus
-)
-
-type TransferOptions struct {
-	S3_endpoint string
-	S3_Bucket   string
-	Md5checksum bool
-}
-
-type TaskTransferConfig struct {
-	S3TransferConfig
-	GlobusTransferConfig
-}
-
-type IngestionTask struct {
-	// DatasetFolderId   uuid.UUID
-	DatasetFolder
-	DatasetMetadata map[string]interface{}
-	TransferMethod  TransferMethod
-	Cancel          context.CancelFunc
-}
-
-type TaskResult struct {
-	Elapsed_seconds int
-	Dataset_PID     string
-	Error           error
-}
-
 func (w *TaskQueue) Startup() {
 
-	w.inputChannel = make(chan IngestionTask)
-	w.resultChannel = make(chan TaskResult)
+	w.inputChannel = make(chan task.IngestionTask)
+	w.resultChannel = make(chan task.Result)
 
 	// start multiple go routines/workers that will listen on the input channel
 	for worker := 1; worker <= w.Config.Misc.ConcurrencyLimit; worker++ {
@@ -64,13 +33,10 @@ func (w *TaskQueue) Startup() {
 
 }
 
-func (w *TaskQueue) CreateTaskFromDatasetFolder(folder DatasetFolder) error {
+func (w *TaskQueue) CreateTaskFromDatasetFolder(folder task.DatasetFolder) error {
 	transferMethod := w.getTransferMethod()
 
-	task := IngestionTask{
-		DatasetFolder:  folder,
-		TransferMethod: transferMethod,
-	}
+	task := task.CreateIngestionTask(folder, map[string]interface{}{}, transferMethod, nil)
 	_, found := w.datasetUploadTasks.Load(task.DatasetFolder.Id)
 	if found {
 		return errors.New("key exists")
@@ -83,12 +49,7 @@ func (w *TaskQueue) CreateTaskFromDatasetFolder(folder DatasetFolder) error {
 
 func (w *TaskQueue) CreateTaskFromMetadata(id uuid.UUID, metadata map[string]interface{}) {
 	transferMethod := w.getTransferMethod()
-
-	task := IngestionTask{
-		DatasetMetadata: metadata,
-		TransferMethod:  transferMethod,
-	}
-
+	task := task.CreateIngestionTask(task.DatasetFolder{}, metadata, transferMethod, nil)
 	w.datasetUploadTasks.Store(id, task)
 }
 
@@ -107,7 +68,7 @@ func (w *TaskQueue) startWorker() {
 func (w *TaskQueue) CancelTask(id uuid.UUID) {
 	value, found := w.datasetUploadTasks.Load(id)
 	if found {
-		f := value.(IngestionTask)
+		f := value.(task.IngestionTask)
 		if f.Cancel != nil {
 			f.Cancel()
 		}
@@ -115,16 +76,18 @@ func (w *TaskQueue) CancelTask(id uuid.UUID) {
 	}
 }
 
-func (w *TaskQueue) RemoveTask(id uuid.UUID) {
+func (w *TaskQueue) RemoveTask(id uuid.UUID) error {
 	value, found := w.datasetUploadTasks.Load(id)
-	if found {
-		f := value.(IngestionTask)
-		if f.Cancel != nil {
-			f.Cancel()
-		}
-		w.datasetUploadTasks.Delete(id)
-		w.Notifier.OnTaskRemoved(id)
+	if !found {
+		return errors.New("task not found")
 	}
+	f := value.(task.IngestionTask)
+	if f.Cancel != nil {
+		f.Cancel()
+	}
+	w.datasetUploadTasks.Delete(id)
+	w.Notifier.OnTaskRemoved(id)
+	return nil
 }
 
 func (w *TaskQueue) ScheduleTask(id uuid.UUID) {
@@ -135,7 +98,7 @@ func (w *TaskQueue) ScheduleTask(id uuid.UUID) {
 		return
 	}
 
-	task := value.(IngestionTask)
+	ingestionTask := value.(task.IngestionTask)
 
 	// Go routine to handle result and errors
 	go func(id uuid.UUID) {
@@ -147,20 +110,28 @@ func (w *TaskQueue) ScheduleTask(id uuid.UUID) {
 			w.Notifier.OnTaskCompleted(id, taskResult.Elapsed_seconds)
 			println(taskResult.Dataset_PID, taskResult.Elapsed_seconds)
 		}
-	}(task.DatasetFolder.Id)
+	}(ingestionTask.DatasetFolder.Id)
 
 	// Go routine to schedule the upload asynchronously
-	go func(folder DatasetFolder) {
+	go func(folder task.DatasetFolder) {
 		fmt.Println("Scheduled upload for: ", folder)
 		w.Notifier.OnTaskScheduled(folder.Id)
 
 		// this channel is read by the go routines that does the actual upload
-		w.inputChannel <- task
-	}(task.DatasetFolder)
-
+		w.inputChannel <- ingestionTask
+	}(ingestionTask.DatasetFolder)
 }
 
-func TestIngestionFunction(task_context context.Context, task IngestionTask, config Config, notifier ProgressNotifier) (string, error) {
+func (w *TaskQueue) GetTaskStatus(id uuid.UUID) (task.TaskStatus, error) {
+	value, found := w.datasetUploadTasks.Load(id)
+	if !found {
+		return task.TaskStatus{}, fmt.Errorf("no task exists with id '%s'", id.String())
+	}
+	t := value.(task.IngestionTask)
+	return t.GetStatus(), nil
+}
+
+func TestIngestionFunction(task_context context.Context, task task.IngestionTask, config Config, notifier ProgressNotifier) (string, error) {
 
 	start := time.Now()
 
@@ -174,21 +145,21 @@ func TestIngestionFunction(task_context context.Context, task IngestionTask, con
 
 }
 
-func (w *TaskQueue) IngestDataset(task_context context.Context, task IngestionTask) TaskResult {
+func (w *TaskQueue) IngestDataset(task_context context.Context, ingestionTask task.IngestionTask) task.Result {
 	start := time.Now()
 	// datasetPID, _ := TestIngestionFunction(task_context, task, w.Config, w.Notifier)
-	datasetPID, err := IngestDataset(task_context, task, w.Config, w.Notifier)
+	datasetPID, err := IngestDataset(task_context, ingestionTask, w.Config, w.Notifier)
 	end := time.Now()
 	elapsed := end.Sub(start)
-	return TaskResult{Dataset_PID: datasetPID, Elapsed_seconds: int(elapsed.Seconds()), Error: err}
+	return task.Result{Dataset_PID: datasetPID, Elapsed_seconds: int(elapsed.Seconds()), Error: err}
 }
 
-func (w *TaskQueue) getTransferMethod() (transferMethod TransferMethod) {
+func (w *TaskQueue) getTransferMethod() (transferMethod task.TransferMethod) {
 	switch strings.ToLower(w.Config.Transfer.Method) {
 	case "globus":
-		transferMethod = TransferGlobus
+		transferMethod = task.TransferGlobus
 	case "s3":
-		transferMethod = TransferS3
+		transferMethod = task.TransferS3
 	}
 	return transferMethod
 }
