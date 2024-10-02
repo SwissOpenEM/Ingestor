5a1b1ed190bdddc3772dde32a2005a7544cf28d6
consolethinks <consolethinks@gmail.com>
2024-10-02 16:08:35 +0200

    add extractuserinfo

--- internal/scicat/userinfo.go
+++ internal/scicat/userinfo.go
@@ -4,61 +4,94 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"log"
 	"net/http"
+	"net/url"
 )
 
-type userInfo struct {
-	CurrentUser      string   `json:"currentUser"`
-	CurrentUserEmail string   `json:"currentUserEmail"`
-	CurrentGroups    []string `json:"currentGroups"`
+type returnedUser struct {
+	Id string `json:"id"`
+}
+
+type userIdentity struct {
+	Profile Profile `json:"profile"`
+}
+
+type Profile struct {
+	Username     string   `json:"username"`
+	DisplayName  string   `json:"displayName"`
+	AccessGroups []string `json:"accessGroups"`
+	Emails       []email  `json:"emails"`
+}
+
+type email struct {
+	Value string `json:"value"`
 }
 
 // this function is temporary until datasetUtils.GetUserInfoFromToken is fixed in scicat-cli
 // TODO; delete this once scicat-cli is updated with fixes
 func getUserInfoFromToken(client *http.Client, APIServer string, token string) (map[string]string, []string, error) {
-	u := make(map[string]string)
+	var newUserInfo returnedUser
 	var accessGroups []string
+	u := map[string]string{}
+	bearerToken := fmt.Sprintf("Bearer %s", token)
 
-	url := APIServer + "/Users/userInfos?access_token=" + token
-	req, err := http.NewRequest("GET", url, nil)
+	// get user info (does not contain access groups) [1st request]
+	req1, err := http.NewRequest("GET", APIServer+"/users/my/self", nil)
 	if err != nil {
 		return map[string]string{}, []string{}, err
 	}
-	req.Header.Set("Content-Type", "application/json")
-
-	resp, err := client.Do(req)
+	req1.Header.Set("Authorization", bearerToken)
+	resp1, err := client.Do(req1)
 	if err != nil {
 		return map[string]string{}, []string{}, err
 	}
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
+	defer resp1.Body.Close()
+	body1, err := io.ReadAll(resp1.Body)
+	if err := json.Unmarshal(body1, &newUserInfo); err != nil {
+		return map[string]string{}, []string{}, err
+	}
+
+	// get extra details about user [2nd request]
+	var respObj userIdentity
+	if err != nil {
+		return map[string]string{}, []string{}, err
+	}
+	filterString := url.QueryEscape(fmt.Sprintf("{\"where\":{\"userId\":\"%s\"}}", newUserInfo.Id))
+	req2, err := http.NewRequest("GET", APIServer+"/useridentities/findOne?filter="+filterString, nil)
+	if err != nil {
+		return map[string]string{}, []string{}, err
+	}
+	req2.Header.Set("Authorization", bearerToken)
+
+	resp2, err := client.Do(req2)
+	if err != nil {
+		return map[string]string{}, []string{}, err
+	}
+	defer resp2.Body.Close()
+	if resp2.StatusCode != 200 {
+		return map[string]string{}, []string{}, fmt.Errorf("could not login with token:%v, status %v", token, resp1.StatusCode)
+	}
+	body2, err := io.ReadAll(resp2.Body)
+	if err != nil {
+		return map[string]string{}, []string{}, err
+	}
+	err = json.Unmarshal(body2, &respObj)
 	if err != nil {
 		return map[string]string{}, []string{}, err
 	}
 
-	if resp.StatusCode != 200 {
-		return map[string]string{}, []string{}, fmt.Errorf("could not login with the current token, status code: \"%v\", status: \"%s\", body: \"%v\"", resp.StatusCode, resp.Status, string(body[:]))
+	// return important user informations
+	if respObj.Profile.Username == "" {
+		return map[string]string{}, []string{}, fmt.Errorf("could not map a user to the token '%v'", token)
 	}
+	u["username"] = respObj.Profile.Username
+	if len(respObj.Profile.Emails) > 0 {
+		u["mail"] = respObj.Profile.Emails[0].Value
+	}
+	u["displayName"] = respObj.Profile.DisplayName
+	u["accessToken"] = token
+	accessGroups = respObj.Profile.AccessGroups
 
-	var respObj userInfo
-	err = json.Unmarshal(body, &respObj)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if respObj.CurrentUser != "" {
-		//log.Printf("Found the following user for this token %v", respObj[0])
-		u["username"] = respObj.CurrentUser
-		u["mail"] = respObj.CurrentUserEmail
-		u["displayName"] = respObj.CurrentUser
-		u["accessToken"] = token
-		log.Printf("User authenticated: %s %s\n", u["displayName"], u["mail"])
-		accessGroups = respObj.CurrentGroups
-		log.Printf("User is member in following groups: %v\n", accessGroups)
-	} else {
-		log.Fatalf("Could not map a user to the token %v", token)
-	}
 	return u, accessGroups, nil
 }
 
