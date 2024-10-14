5332cec52261ab1dff65eeb063856eda797c19fb
Philipp Wissmann <philipp.wissmann@ethz.ch>
2024-10-14 15:20:05 +0200

    feat(logging): add structured logging to task queue

---
+++ internal/core/loggingnotifier.go
@@ -0,0 +1,31 @@
+package core
+
+import (
+	"log/slog"
+
+	"github.com/google/uuid"
+)
+
+type LoggingNotifier struct{}
+
+func (n *LoggingNotifier) OnTaskScheduled(id uuid.UUID) {
+	slog.Info("Task scheduled", "id", id)
+}
+func (n *LoggingNotifier) OnTaskCanceled(id uuid.UUID) {
+	slog.Info("Task canceled", "id", id)
+}
+func (n *LoggingNotifier) OnTaskAdded(id uuid.UUID, folder string) {
+	slog.Info("Task added", "id", id, "folder", folder)
+}
+func (n *LoggingNotifier) OnTaskRemoved(id uuid.UUID) {
+	slog.Info("Task removed", "id", id)
+}
+func (n *LoggingNotifier) OnTaskFailed(id uuid.UUID, err error) {
+	slog.Error("Task failed", "id", id, "err", err.Error())
+}
+func (n *LoggingNotifier) OnTaskCompleted(id uuid.UUID, elapsedSeconds int) {
+	slog.Info("Task completed", "id", id, "elapsed", elapsedSeconds)
+}
+func (n *LoggingNotifier) OnTaskProgress(id uuid.UUID, currentFile int, totalFiles int, elapsedSeconds int) {
+	slog.Info("Task progress", "id", id, "percentage", currentFile/totalFiles*100, "elapsed", elapsedSeconds)
+}
