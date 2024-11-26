147654b1ddc39c120233b242c157d615ab83f509
consolethinks <consolethinks@gmail.com>
2024-11-26 12:37:11 +0100

    add rbac authorization

--- internal/webserver/keycloakclaims.go
+++ internal/webserver/keycloakclaims.go
@@ -1,7 +1,33 @@
 package webserver
 
-type claims struct {
-	Email             string `json:"email"`
-	EmailVerified     bool   `json:"email_verified"`
-	PreferredUsername string `json:"preferred_username"`
+import "github.com/golang-jwt/jwt/v5"
+
+type rolesList struct {
+	Roles []string `json:"roles,omitempty"`
+}
+
+type keycloakClaims struct {
+	RealmAccess    rolesList            `json:"realm_access,omitempty"`
+	ResourceAccess map[string]rolesList `json:"resource_access,omitempty"`
+	jwt.RegisteredClaims
+}
+
+func (c *keycloakClaims) GetRealmRoles() []string {
+	return c.RealmAccess.Roles
+}
+
+func (c *keycloakClaims) GetResourceRolesByKey(key string) []string {
+	if val, ok := c.ResourceAccess[key]; ok {
+		return val.Roles
+	} else {
+		return []string{}
+	}
+}
+
+func (c *keycloakClaims) GetAllResourceRoles() map[string][]string {
+	returnMap := make(map[string][]string)
+	for k, v := range c.ResourceAccess {
+		returnMap[k] = v.Roles
+	}
+	return returnMap
 }
