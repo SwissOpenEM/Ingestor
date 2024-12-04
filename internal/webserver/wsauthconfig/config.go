9386a24b782b83aebf79e955cbfbb4d8e36fdf31
Consolethinks <wewa@first-email.net>
2024-12-04 14:00:37 +0100

    add config validation

--- internal/webserver/wsauthconfig/config.go
+++ internal/webserver/wsauthconfig/config.go
@@ -2,18 +2,18 @@ package wsauthconfig
 
 type OAuth2Conf struct {
 	ClientID     string   `validate:"required"` // OAuth client id (this app)
-	ClientSecret string   `validate:"required"` // OAuth2 secret (associated with ClientID, optional)
+	ClientSecret string   // OAuth2 secret (associated with ClientID, optional)
 	RedirectURL  string   `validate:"required"` // where should the OAuth2 provider return the user to
 	Scopes       []string // list of scopes to ask for from the OAuth2 provider
 }
 
 type OIDCConf struct {
-	IssuerURL string `string:"IssuerURL"`
+	IssuerURL string `validate:"url,omitempty"`
 
 	// the ones below are only needed if the OIDC discovery mechanism doesn't work
-	AuthURL     string
-	TokenURL    string
-	UserInfoURL string
+	AuthURL     string `validate:"omitempty,url"`
+	TokenURL    string `validate:"omitempty,url"`
+	UserInfoURL string `validate:"omitempty,url"`
 	Algorithms  []string
 }
 
@@ -21,7 +21,7 @@ type OIDCConf struct {
 type JWTConf struct {
 	UseJWKS bool `bool:"UseJWKS" validate:"required"`
 	// used when UseJWKS is set to true
-	JwksURL              string   `validate:"required_if=UseJWKS true,omitempty"`
+	JwksURL              string   `validate:"required_if=UseJWKS true,url,omitempty"`
 	JwksSignatureMethods []string `validate:"required_if=UseJWKS true,omitempty"`
 	// used when UseJWKS is set to false
 	Key           string `validate:"required_if=UseJWKS false,omitempty"`                                                                   // public key in case of asymmetric method, otherwise common secret (HMAC)
@@ -37,7 +37,7 @@ type RBACConf struct {
 
 // full authentication config
 type AuthConf struct {
-	Disable         bool `bool:"Disable" validate:"required"`
+	Disable         bool `bool:"Disable"`
 	SessionDuration uint // duration of a user session before it expires (by default never)
 	OAuth2Conf      `mapstructure:"OAuth2" validate:"required_if=Disable false,omitempty"`
 	OIDCConf        `mapstructure:"OIDC" validate:"required_if=Disable false,omitempty"`
