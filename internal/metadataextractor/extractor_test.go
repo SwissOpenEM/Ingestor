0dad190b962e4d945680847b7f8acb3bd86c00d4
consolethinks <consolethinks@gmail.com>
2024-12-12 13:55:33 +0100

    fix extractor test

--- internal/metadataextractor/extractor_test.go
+++ internal/metadataextractor/extractor_test.go
@@ -8,7 +8,9 @@ import (
 	"os"
 	"path"
 	"reflect"
+	"runtime"
 	"testing"
+	"time"
 )
 
 func TestNewExtractorHandler(t *testing.T) {
@@ -309,9 +311,16 @@ func Test_runExtractor(t *testing.T) {
 
 func TestExtractorHandler_ExtractMetadata(t *testing.T) {
 	// TODO: make platform independent
-	templ, _ := template.New("name").Parse("{{.OutputFile}}")
+	templ, _ := template.New("name").Parse("{} {{.OutputFile}}")
+	_, goFile, _, ok := runtime.Caller(0)
+	if !ok {
+		return // skip test if can't get path
+	}
+	currDir := path.Dir(goFile)
+	execPath := path.Join(currDir, "extractor_test_echoToFile.sh")
 
 	type fields struct {
+		methods      map[string]Method
 		extractors   map[string]Extractor
 		outputFolder string
 	}
@@ -328,29 +337,32 @@ func TestExtractorHandler_ExtractMetadata(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name: "touchExtractor",
+			name: "echoExtractor",
 			fields: fields{
+				methods: map[string]Method{"echoExtractor": {Name: "echoExtractor", Schema: "someschema", Extractor: "echoExtractor"}},
 				extractors: map[string]Extractor{
-					"touchExtractor": {
-						ExecutablePath: "touch",
+					"echoExtractor": {
+						ExecutablePath: execPath,
 						templ:          templ,
 					},
 				},
 			},
 			args: args{
-				extractor_name: "touchExtractor",
+				extractor_name: "echoExtractor",
 				folder:         "./",
 				output_file:    path.Join(os.TempDir(), "output.txt"),
 			},
-			want:    "", // size of a directory on linux
+			want:    "{}\n", // size of a directory on linux
 			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			e := &ExtractorHandler{
+				methods:      tt.fields.methods,
 				extractors:   tt.fields.extractors,
 				outputFolder: tt.fields.outputFolder,
+				timeout:      time.Minute,
 			}
 			ctx := context.Background()
 			got, err := e.ExtractMetadata(ctx, tt.args.extractor_name, tt.args.folder, tt.args.output_file, stdout_callback, stderr_callback)
