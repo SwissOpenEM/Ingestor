78f061a64c2e435c6e97b26955f5aef01fc1ecb5
consolethinks <consolethinks@gmail.com>
2025-02-21 17:30:40 +0100

    move transfer notifier to own file

--- internal/core/ingestdataset.go
+++ internal/core/ingestdataset.go
@@ -18,48 +18,11 @@ import (
 	"github.com/SwissOpenEM/Ingestor/internal/transfer"
 	"github.com/SwissOpenEM/globus"
 	"github.com/fatih/color"
-	"github.com/google/uuid"
 	"github.com/paulscherrerinstitute/scicat-cli/v3/datasetIngestor"
 	"github.com/paulscherrerinstitute/scicat-cli/v3/datasetUtils"
 )
 
 const MAX_FILES = 400000
-
-type TransferTaskNotifier struct {
-	id    uuid.UUID
-	qn    progress.QueueNotifier
-	tt    *task.TransferTask
-	start time.Time
-}
-
-func createTransferTaskNotifier(id uuid.UUID, qn progress.QueueNotifier, tt *task.TransferTask, start time.Time) TransferTaskNotifier {
-	return TransferTaskNotifier{
-		id:    id,
-		qn:    qn,
-		tt:    tt,
-		start: start,
-	}
-}
-
-func (n TransferTaskNotifier) OnTransferProgress(bytesTransferred int, filesTransferred int) {
-	n.qn.OnTaskProgress(n.id, filesTransferred, n.tt.GetDetails().FilesTotal, int(time.Since(n.start).Seconds()))
-	n.tt.UpdateDetails(task.SetBytesTransferred(bytesTransferred), task.SetFilesTransferred(filesTransferred))
-}
-
-func (n TransferTaskNotifier) OnTransferCompleted() {
-	n.qn.OnTaskCompleted(n.id, int(time.Since(n.start).Seconds()))
-	n.tt.UpdateDetails(task.SetStatus(task.Finished), task.SetMessage("finished"))
-}
-
-func (n TransferTaskNotifier) OnTransferCancelled() {
-	n.qn.OnTaskCanceled(n.id)
-	n.tt.UpdateDetails(task.SetStatus(task.Cancelled), task.SetMessage("cancelled"))
-}
-
-func (n TransferTaskNotifier) OnTransferFailed(err error) {
-	n.qn.OnTaskFailed(n.id, err)
-	n.tt.UpdateDetails(task.SetStatus(task.Failed), task.SetMessage(fmt.Sprintf("task failed with the following error: %s", err.Error())))
-}
 
 func createLocalSymlinkCallbackForFileLister(skipSymlinks *string, skippedLinks *uint) func(symlinkPath string, sourceFolder string) (bool, error) {
 	scanner := bufio.NewScanner(os.Stdin)
---
+++ internal/core/transfernotifier.go
@@ -0,0 +1,46 @@
+package core
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/SwissOpenEM/Ingestor/internal/progress"
+	"github.com/SwissOpenEM/Ingestor/internal/task"
+	"github.com/google/uuid"
+)
+
+type TransferTaskNotifier struct {
+	id    uuid.UUID
+	qn    progress.QueueNotifier
+	tt    *task.TransferTask
+	start time.Time
+}
+
+func createTransferTaskNotifier(id uuid.UUID, qn progress.QueueNotifier, tt *task.TransferTask, start time.Time) TransferTaskNotifier {
+	return TransferTaskNotifier{
+		id:    id,
+		qn:    qn,
+		tt:    tt,
+		start: start,
+	}
+}
+
+func (n TransferTaskNotifier) OnTransferProgress(bytesTransferred int, filesTransferred int) {
+	n.qn.OnTaskProgress(n.id, filesTransferred, n.tt.GetDetails().FilesTotal, int(time.Since(n.start).Seconds()))
+	n.tt.UpdateDetails(task.SetBytesTransferred(bytesTransferred), task.SetFilesTransferred(filesTransferred))
+}
+
+func (n TransferTaskNotifier) OnTransferCompleted() {
+	n.qn.OnTaskCompleted(n.id, int(time.Since(n.start).Seconds()))
+	n.tt.UpdateDetails(task.SetStatus(task.Finished), task.SetMessage("finished"))
+}
+
+func (n TransferTaskNotifier) OnTransferCancelled() {
+	n.qn.OnTaskCanceled(n.id)
+	n.tt.UpdateDetails(task.SetStatus(task.Cancelled), task.SetMessage("cancelled"))
+}
+
+func (n TransferTaskNotifier) OnTransferFailed(err error) {
+	n.qn.OnTaskFailed(n.id, err)
+	n.tt.UpdateDetails(task.SetStatus(task.Failed), task.SetMessage(fmt.Sprintf("task failed with the following error: %s", err.Error())))
+}
