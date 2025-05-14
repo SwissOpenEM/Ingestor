5642a47dd10d0e7eb3363d2c8ff983ab002158d5
consolethinks <consolethinks@gmail.com>
2025-05-14 17:21:51 +0200

    access control WIP 2

--- internal/datasetaccess/checkaccess.go
+++ internal/datasetaccess/checkaccess.go
@@ -1,6 +1,7 @@
 package datasetaccess
 
 import (
+	"errors"
 	"os"
 	"path/filepath"
 	"strings"
@@ -10,7 +11,64 @@ import (
 
 const accessControlFilename = "openem-access.yaml"
 
-func CheckAccess(path string, hasGroups []string) error {
+func parseAccessFile(path string) (accessFile, error) {
+	rawAccessFile, err := os.ReadFile(path)
+	if err != nil {
+		return accessFile{}, err
+	}
+
+	var parsedAccessFile accessFile
+	err = yaml.Unmarshal(rawAccessFile, &parsedAccessFile)
+	return parsedAccessFile, err
+}
+
+func CheckAccessIntegrity(path string) error {
+	splitPath := strings.Split(filepath.Clean(path), string(filepath.Separator))
+	allFoundGroups := map[string]bool{}
+
+	// check if the access hierarchy follows the rules by traversing parent directories
+	for i := len(splitPath); i > 0; i-- {
+		/// read and parse file
+		path := filepath.Join(append(splitPath[0:i], accessControlFilename)...)
+
+		if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
+			continue // skip the path if it doesn't contain any access restrictions
+		}
+
+		parsedAccessFile, err := parseAccessFile(path)
+		if err != nil {
+			return err
+		}
+
+		// path rule check
+		if parsedAccessFile.Path != path {
+			return newPathError(parsedAccessFile.Path, path)
+		}
+
+		parsedGroups := map[string]bool{}
+		for _, group := range parsedAccessFile.AccessGroups {
+			parsedGroups[group] = true
+		}
+
+		invalidGroups := []string{}
+		for group := range allFoundGroups {
+			if _, ok := parsedGroups[group]; !ok {
+				invalidGroups = append(invalidGroups, group)
+			}
+		}
+		if len(invalidGroups) > 0 { // return an error in case of any mismatched groups
+			return newInvalidGroupsError(path, invalidGroups)
+		}
+
+		// update map of all encountered groups
+		for group := range parsedGroups {
+			allFoundGroups[group] = true
+		}
+	}
+	return nil
+}
+
+func CheckUserAccess(path string, hasGroups []string) error {
 	splitPath := strings.Split(filepath.Clean(path), string(filepath.Separator))
 
 	allowedGroups := map[string]bool{}
@@ -19,34 +77,27 @@ func CheckAccess(path string, hasGroups []string) error {
 		hasGroupsMap[group] = true
 	}
 
-	firstFile := true
-	for i := len(splitPath) - 1; i >= 0; i-- {
+	for i := len(splitPath); i > 0; i-- {
+		/// read and parse file
 		path := filepath.Join(append(splitPath[0:i], accessControlFilename)...)
-		rawAccessFile, err := os.ReadFile(path)
+
+		if _, err := os.Stat(path); errors.Is(err, os.ErrNotExist) {
+			continue // skip the path if it doesn't contain any access restrictions
+		}
+
+		parsedAccessFile, err := parseAccessFile(path)
 		if err != nil {
 			return err
 		}
 
-		var parsedAccessFile accessFile
-		yaml.Unmarshal(rawAccessFile, &parsedAccessFile)
-		if firstFile {
-			firstFile = false
-			for _, group := range parsedAccessFile.AccessGroups {
-				allowedGroups[group] = true
-			}
-		} else {
-			invalidGroups := []string{}
-			for _, group := range parsedAccessFile.AccessGroups {
-				if _, ok := allowedGroups[group]; !ok {
-					invalidGroups = append(invalidGroups, group)
-				}
-			}
-			if len(invalidGroups) > 0 {
-				return NewInvalidGroupsError(path, invalidGroups)
-			}
+		// the first file closest to the final path determines the strictest level of required groups
+		for _, group := range parsedAccessFile.AccessGroups {
+			allowedGroups[group] = true
 		}
+		break // no need to continue iterate further, we got the (theoretical) strictest set of groups
 	}
 
+	// check if user has required groups
 	allowedGroupsSlice := make([]string, 0, len(allowedGroups))
 	for group := range allowedGroups {
 		if _, ok := hasGroupsMap[group]; ok {
@@ -55,5 +106,5 @@ func CheckAccess(path string, hasGroups []string) error {
 		allowedGroupsSlice = append(allowedGroupsSlice, group)
 	}
 
-	return NewAccessError(allowedGroupsSlice)
+	return newAccessError(allowedGroupsSlice)
 }
--- internal/datasetaccess/errors.go
+++ internal/datasetaccess/errors.go
@@ -10,7 +10,7 @@ func (e *AccessError) Error() string {
 	return fmt.Sprintf("the user does not have access to the datasets, one of the following groups is needed: %v", e.groupsWithAccess)
 }
 
-func NewAccessError(groupsWithAccess []string) *AccessError {
+func newAccessError(groupsWithAccess []string) *AccessError {
 	return &AccessError{
 		groupsWithAccess: groupsWithAccess,
 	}
@@ -25,9 +25,25 @@ func (e *InvalidGroupsError) Error() string {
 	return fmt.Sprintf("the following invalid groups were found when checking the path '%s': '%v'", e.currentPath, e.invalidGroups)
 }
 
-func NewInvalidGroupsError(path string, groups []string) *InvalidGroupsError {
+func newInvalidGroupsError(path string, groups []string) *InvalidGroupsError {
 	return &InvalidGroupsError{
 		currentPath:   path,
 		invalidGroups: groups,
+	}
+}
+
+type PathError struct {
+	yamlPath   string
+	actualPath string
+}
+
+func (e *PathError) Error() string {
+	return fmt.Sprintf("the path indicated in the '%s' access file is different from the actual path - got: '%s', wanted: '%s'", accessControlFilename, e.yamlPath, e.actualPath)
+}
+
+func newPathError(yamlPath string, actualPath string) *PathError {
+	return &PathError{
+		yamlPath:   yamlPath,
+		actualPath: actualPath,
 	}
 }
