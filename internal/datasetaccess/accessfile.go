5844cf1830abe910ca16ac2272265db3f46ce075
consolethinks <consolethinks@gmail.com>
2025-06-19 16:24:28 +0200

    remove path from access file

--- internal/datasetaccess/accessfile.go
+++ internal/datasetaccess/accessfile.go
@@ -1,7 +1,6 @@
 package datasetaccess
 
 type accessFile struct {
-	Path              string   `yaml:"Path"`
 	HasDatasetFolders bool     `yaml:"HasDatasetFolders"`
 	AllowedGroups     []string `yaml:"AllowedGroups"`
 	BlockedGroups     []string `yaml:"BlockedGroups"`
